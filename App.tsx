cbd3904c96316003e76cfbe8a617c00cae428e56
ws-cho <59313381+ws-cho@users.noreply.github.com>
2022-09-22 17:36:19 +0900

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './style.css';
+import { useState } from 'react';
 
 function Header(props) {
   return (
@@ -14,32 +15,48 @@ function Header(props) {
 function Nav(props) {
   const list = [];
   for (let i = 0; i < props.topics.length; i++) {
-    list.push(props.topics[i]);
+    let elem = props.topics[i];
+    list.push(
+      <li>
+        <a href={'#' + elem.id}>{elem.title}</a>
+      </li>
+    );
   }
-  return <nav></nav>;
+  return (
+    <nav>
+      <ol>{list}</ol>
+    </nav>
+  );
 }
 
 function Article(props) {
   return (
     <article>
-      <h2> {props.title}</h2>
+      <h2>{props.title}</h2>
       <p>{props.body}</p>
     </article>
   );
 }
 
 export default function App() {
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+  let content = null;
   const topics = [
     { id: 1, title: 'html', body: 'html ...' },
     { id: 1, title: 'css', body: 'css ...' },
     { id: 1, title: 'javascript', body: 'javascript ...' },
   ];
-  let content = 'Content Space';
+  if (mode === 'WELCOME') {
+    content = <Article title="Welcome" body="Hello WEB"></Article>;
+  } else if (mode === 'READ') {
+    content = <Article title="Read" body="Hello READ"></Article>;
+  }
+
   return (
     <div>
       <Header title="WEB"></Header>
       <Nav topics={topics}></Nav>
-      <Article title="Welcome" body="Hello WEB"></Article>
       {content}
     </div>
   );
